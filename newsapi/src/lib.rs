d0b5b583689b554b8e1794008d7016f20ac5de3e
Ryan Yogan <ryanyogan@gmail.com>
2023-12-24 13:30:24 -0600

    feat // switched to workspace, imported newsapi

---
+++ newsapi/src/lib.rs
@@ -0,0 +1,161 @@
+#[cfg(feature = "async")]
+use reqwest::Method;
+
+use serde::Deserialize;
+use url::Url;
+
+const BASE_URL: &str = "https://newsapi.org/v2/";
+
+#[derive(thiserror::Error, Debug)]
+pub enum NewsApiError {
+    #[error("Failed fetching articles")]
+    RequestFailed(#[from] ureq::Error),
+    #[error("Failed converting response to string")]
+    FailedResponseToString(#[from] std::io::Error),
+    #[error("Article Parsing Failed ... Ah Shit")]
+    ArticleParseFailed(#[from] serde_json::Error),
+    #[error("URL parsing failed")]
+    UrlParsing(#[from] url::ParseError),
+    #[error("Request failed: {0}")]
+    BadRequest(&'static str),
+    #[error("Async request failed: {0}")]
+    #[cfg(feature = "async")]
+    AsyncRequestFailed(#[from] reqwest::Error),
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct NewsAPIResponse {
+    pub articles: Vec<Article>,
+    status: String,
+    code: Option<String>,
+}
+
+impl NewsAPIResponse {
+    pub fn articles(&self) -> &Vec<Article> {
+        &self.articles
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Article {
+    pub title: String,
+    pub url: String,
+}
+
+impl Article {
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    pub fn url(&self) -> &str {
+        &self.url
+    }
+}
+
+pub enum Endpoint {
+    TopHeadlines,
+}
+
+impl ToString for Endpoint {
+    fn to_string(&self) -> String {
+        match self {
+            Self::TopHeadlines => "top-headlines".to_string(),
+        }
+    }
+}
+
+pub enum Country {
+    Us,
+}
+
+impl ToString for Country {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Us => "us".to_string(),
+        }
+    }
+}
+
+pub struct NewsAPI {
+    api_key: String,
+    endoint: Endpoint,
+    country: Country,
+}
+
+impl NewsAPI {
+    pub fn new(api_key: &str) -> Self {
+        Self {
+            api_key: api_key.to_string(),
+            endoint: Endpoint::TopHeadlines,
+            country: Country::Us,
+        }
+    }
+
+    pub fn endpoint(&mut self, endpoint: Endpoint) -> &mut NewsAPI {
+        self.endoint = endpoint;
+        self
+    }
+
+    pub fn country(&mut self, country: Country) -> &mut NewsAPI {
+        self.country = country;
+        self
+    }
+
+    fn prepare_url(&self) -> Result<String, NewsApiError> {
+        let mut url = Url::parse(BASE_URL)?;
+        url.path_segments_mut()
+            .unwrap()
+            .push(&self.endoint.to_string());
+
+        let country = format!("country={}", self.country.to_string());
+        url.set_query(Some(&country));
+
+        Ok(url.to_string())
+    }
+
+    pub fn fetch(&self) -> Result<NewsAPIResponse, NewsApiError> {
+        let url = self.prepare_url()?;
+        let req = ureq::get(&url).set("Authorization", &self.api_key);
+        let response = req.call()?.into_json::<NewsAPIResponse>()?;
+
+        match response.status.as_str() {
+            "ok" => return Ok(response),
+            _ => return Err(map_response_error(response.code)),
+        }
+    }
+
+    #[cfg(feature = "async")]
+    pub async fn fetch_async(&self) -> Result<NewsAPIResponse, NewsApiError> {
+        let url = self.prepare_url()?;
+        let client = reqwest::Client::new();
+        let request = client
+            .request(Method::GET, url)
+            .header("Authorization", &self.api_key)
+            .build()
+            .map_err(|e| NewsApiError::AsyncRequestFailed(e))?;
+
+        let response = client
+            .execute(request)
+            .await?
+            .json::<NewsAPIResponse>()
+            .await
+            .map_err(|e| NewsApiError::AsyncRequestFailed(e))?;
+
+        match response.status.as_str() {
+            "ok" => return Ok(response),
+            _ => return Err(map_response_error(response.code)),
+        }
+    }
+}
+
+fn map_response_error(code: Option<String>) -> NewsApiError {
+    if let Some(code) = code {
+        match code.as_str() {
+            "apiKeyDisabled" => NewsApiError::BadRequest("Your API key has been disabled"),
+            _ => NewsApiError::BadRequest("Unknown Error"),
+        }
+    } else {
+        NewsApiError::BadRequest("Unknown Error")
+    }
+}
