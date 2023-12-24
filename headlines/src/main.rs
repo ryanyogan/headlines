c6913d94d8d5da2c084529b03ae1a197c1398196
Ryan Yogan <ryanyogan@gmail.com>
2023-12-24 16:28:23 -0600

    feat // k.. we got a shell

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,3 +1,89 @@
+use std::borrow::Cow;
+
+use eframe::{
+    egui::{CentralPanel, FontDefinitions, FontFamily, ScrollArea, Vec2},
+    epi::App,
+    run_native, NativeOptions,
+};
+
+struct Headlines {
+    articles: Vec<NewsCardData>,
+}
+
+impl Headlines {
+    fn new() -> Self {
+        let iter = (0..20).map(|a| NewsCardData {
+            title: format!("title{}", a),
+            desc: format!("desc{}", a),
+            url: format!("https://example.com/{}", a),
+        });
+
+        Self {
+            articles: Vec::from_iter(iter),
+        }
+    }
+
+    fn configure_fonts(&self, ctx: &eframe::egui::CtxRef) {
+        let mut font_def = FontDefinitions::default();
+        font_def.font_data.insert(
+            "MesloLGS".to_string(),
+            Cow::Borrowed(include_bytes!("../../MesloLGS_NF_Regular.ttf")),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Heading,
+            (FontFamily::Proportional, 35.),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Body,
+            (FontFamily::Proportional, 20.),
+        );
+        font_def
+            .fonts_for_family
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "MesloLGS".to_string());
+
+        ctx.set_fonts(font_def)
+    }
+}
+
+struct NewsCardData {
+    title: String,
+    desc: String,
+    url: String,
+}
+
+impl App for Headlines {
+    fn setup(
+        &mut self,
+        ctx: &eframe::egui::CtxRef,
+        _frame: &mut eframe::epi::Frame<'_>,
+        _storage: Option<&dyn eframe::epi::Storage>,
+    ) {
+        self.configure_fonts(ctx);
+    }
+    fn update(&mut self, ctx: &eframe::egui::CtxRef, _frame: &mut eframe::epi::Frame<'_>) {
+        CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::auto_sized().show(ui, |ui| {
+                for a in &self.articles {
+                    ui.label(&a.title);
+                    ui.label(&a.desc);
+                    ui.label(&a.url);
+                }
+            })
+        });
+    }
+
+    fn name(&self) -> &str {
+        "Headlines"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut win_options = NativeOptions::default();
+    win_options.initial_window_size = Some(Vec2::new(540., 960.));
+
+    let app = Headlines::new();
+
+    run_native(Box::new(app), win_options)
 }
